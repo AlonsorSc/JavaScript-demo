2ba9bd52758db4c042ecd0b57dee1226f7e23c09
Nikita <altorego1@yandex.ru>
2025-07-29 14:46:28 +0300

    master - 4.7. Шаблонные строки

--- app.js
+++ app.js
@@ -7,3 +7,17 @@ console.log('Стоимость работ: ' + projectHours * payRateUSD + '$')
 
 
 
+const projectName = 'cайт магазина';
+const price = 2000;
+const author = 'Василий Пупкин';
+
+const template2 = `${author} заказал ${projectName} по цене ${price} руб`;
+console.log(template2);
+
+const template3 = 'Проект \n' + 'Цена: ' + price + '$';
+console.log(template3);
+
+const template4 = `Проект
+Цена: ${price}$`;
+console.log(template4);
+
