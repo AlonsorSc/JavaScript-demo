a8f63e51c6a2f5e19eaa15986b449716bce6c350
Nikita <altorego1@yandex.ru>
2025-07-29 13:13:32 +0300

    master - 4.6. Упражнение - Расчёт проекта

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log('Hello, World!');
+const payRateUSD = 80;
+const projectHours = 40;
+const availableHours = (11 - 2) * 5;
+
+console.log('Смогу ли я работать? ' + (availableHours > projectHours));
+console.log('Стоимость работ: ' + projectHours * payRateUSD + '$');
+
+
+
