93546fa325c310b41fe1931784e5fdf704a99b44
Nikita <altorego1@yandex.ru>
2025-07-29 20:27:17 +0300

    master - 4.9 - 4.11 False из других типов + занятие и тест

--- app.js
+++ app.js
@@ -1,23 +1,6 @@
-const payRateUSD = 80;
-const projectHours = 40;
-const availableHours = (11 - 2) * 5;
-
-console.log('Смогу ли я работать? ' + (availableHours > projectHours));
-console.log('Стоимость работ: ' + projectHours * payRateUSD + '$');
-
-
-
-const projectName = 'cайт магазина';
-const price = 2000;
-const author = 'Василий Пупкин';
-
-const template2 = `${author} заказал ${projectName} по цене ${price} руб`;
-console.log(template2);
-
-const template3 = 'Проект \n' + 'Цена: ' + price + '$';
-console.log(template3);
-
-const template4 = `Проект
-Цена: ${price}$`;
-console.log(template4);
-
+console.log(Boolean(0));
+console.log(Boolean(''));
+console.log(Boolean(undefined));
+console.log(Boolean(null));
+console.log(Boolean(Number('sx')));
+console.log(Boolean(NaN));
