fa5aeddb7ebccaed35cb965ca7fa0da7137fe493
Nikita <altorego1@yandex.ru>
2025-08-04 21:47:07 +0300

    master - 7.4. Упражнение - Упрощение функции

--- 1 - Упражнения/app.js
+++ 1 - Упражнения/app.js
@@ -1,20 +1,9 @@
-/*
-	Пользователь хочет приобрести игру в магазине
-	Он может это сделать только если:
-	- Eго баланс больше 1000 (balance) 
-	или число бонусов больше 100 (bonusBalance)
-	- Он не забанен (isBanned)
-	- Игра не кулена (isExist)
-	- Игра в продаже (isSelling)
-	Напишите условие для покупки и выведите в консоль
-	результат
-*/
+function toPower(num, power) {
+	const res = num ** power;
+	return res;
+}
+console.log(toPower(2, 3));
 
-const balance = 900;
-const bonusBalance = 90;
-let isBanned = false;
-let isExist = false;
-let isSelling = true;
-const canBuy = (balance >= 1000 || bonusBalance >= 100) && !isBanned && !isExist && isSelling;
-
-console.log(`Могу купить игру: ${canBuy ? 'Да' : 'Нет'}`);
+// Перевели обычную функцию в стрелочную
+const toPowerArrow = (num, power) => num ** power;
+console.log(toPowerArrow(2, 4));
