d48c9704e13d5773b0676695084a0a17624cea49
Nikita <altorego1@yandex.ru>
2025-08-04 22:17:26 +0300

    master - 7.6. Параметры по умолчанию

--- app.js
+++ app.js
@@ -1,33 +1,44 @@
-// Обычная функция
-function powerOfTwo(num) {
-    return num * num;
+function toPower(num, power) {
+    console.log(typeof power);
+	const res = num ** power;
+	return res;
 }
-console.log(powerOfTwo(5));
+console.log(toPower(2, 3));
 
-// Стрелочная функция
-const poft = (num) => num * num;
-console.log(poft(6));
+/* В данном примере мы забыли указать вывод второго аргумента функции.
+Не заданный второй аргумент является undefined.
+Tсли мы число num возведём в степень power, т.е. в undefinder, 
+то получим NaN (Not a Number).*/
+console.log(toPower(2));
 
-/* Стрелочная функция можно аргумент без скобки 
-То, что до стрелки - аргументы, после стрелки то, 
-что функция будет возвращать. */
-const poft2 = num => num * num;
-console.log(poft2(7));
-
-/* Сайдэфект логирования в обычной и стрелочной функции.
-В обычной функции:*/
-function powerOfTwo(num) {
-    console.log(num);
-    return num * num;
+/* Булевая логика и применение оператора нулевого слияния
+Для исправления ситуации, когда мы ввели пустой аргумент,
+мы применяем оператор нулевого слияния. При помощи ?? если у нас power будет пустой,
+тогда он применит 2. Т.к. обычная функци плавующая, то console.log может быть наверху и
+отрабатываться. */
+function toPower(num, power) {
+    const power2 = power ?? 2;
+    const res = num ** power2; 
+	return res;
 }
-console.log(powerOfTwo(5));
-// В стрелочной функции:
-const poft3 = num => {
-    console.log(num);
-    return num * num;
-};
-console.log(poft3(8));
 
-// Несколько аргументов стрелочной функции в скобках ()
-const poft4 = (num, i) => num * num;
-console.log(poft4(7));
+/* Упрощение предыдущего варианта. 
+Т.к. по приоритету идёт сначала оператор нулевого присваивания, то
+сначала JS выбирает из power ?? 2, а затем по очереди применяется
+присваивание через = и полученный результат присваевается переменной power в зависимости от
+того, что пришло в аргумент. Если аргумент power пустой, то будет 2, если цифра, то power = power. */
+function toPower2(num, power) {
+    power = power ?? 2;
+    const res = num ** power; 
+	return res;
+}
+console.log(toPower2(3));
+
+/* Установка дефолтного значения для функции без лишних строк power = power ?? 2; 
+Если мы не зададим в выводе функции нужный аргумент, то дефолтный будет 2,
+т.к. мы его задали в аргументах функции. */ 
+function toPower3(num, power = 2) {
+    const res = num ** power; 
+	return res;
+}
+console.log(toPower3(4));
