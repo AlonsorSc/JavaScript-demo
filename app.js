692ee1a5c573b7b3d35207b4bf1c0c49ff334097
Nikita <altorego1@yandex.ru>
2025-08-06 20:26:42 +0300

    master - 7.8. Практика п.7.1. введение в функции

--- app.js
+++ app.js
@@ -1,28 +1,8 @@
-const KG_IN_USD = 7;
-const KM_IN_USD = 5;
+function test(a) {
+    if (a / 2 == a / 0) {
+        console.log('Число чётное');
+    } else () {
 
-function calculateW(present) {
-    return present * KG_IN_USD;
+    }
 }
-
-function calculateKm(distance) {
-    return distance * KM_IN_USD;
-}
-
-// Простой вариант без использования функции в функции
-function getExchangePrice(present1, present2, distance) {
-    const price1 = present1 * KG_IN_USD;
-    const price2 = present2 * KG_IN_USD;
-    const distancePrice = distance * KM_IN_USD;
-    return price1 + price2 + distancePrice;
-}
-console.log(getExchangePrice(1, 2, 10));
-
-// Вариант сложнее с использованием функции в функции
-function getExchangePrice(present1, present2, distance) {
-    const price1 = calculateW(present1);
-    const price2 = calculateW(present2);
-    const distancePrice = calculateKm(distance);
-    return price1 + price2 + distancePrice;
-}
-console.log(getExchangePrice(1, 2, 10));
+console.log(test());
