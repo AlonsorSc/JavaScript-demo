7f4881a4f3c0aea877c63dc131eb7a24921cd2d4
Nikita <altorego1@yandex.ru>
2025-08-08 13:09:39 +0300

    6-drive - 6.8. Домашнее задание - Булева логика - убрал 1 сложный вариант через switch.

--- 2 - Домашние задания/6-driver/6-driver.js
+++ 2 - Домашние задания/6-driver/6-driver.js
@@ -3,14 +3,5 @@ const hasLicence = true;
 const age = 18;
 const isDrunk = false;
 
-switch (true) {
-    case hasLicence && age>=18 && !isDrunk:
-        console.log('Машину вести может');
-        break;
-    default:
-        console.log('Не может водить машину');
-}
-
-// 2 вариант
 let canDrive = hasLicence === true && age>=18 && isDrunk === false ? 'Да' : 'Нет';
 console.log(`Можно ли водить машину? ${canDrive}`);
