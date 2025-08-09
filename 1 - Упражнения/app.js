083bf142d3fbfb64a25b30903aa274868ac1dff0
Nikita <altorego1@yandex.ru>
2025-08-09 13:08:20 +0300

    master - настройка prettier

--- 1 - Упражнения/app.js
+++ 1 - Упражнения/app.js
@@ -1,33 +1,9 @@
 /*
-	Пользователь:
-	- Возраст
-	- Наличие работы
-	- Деньги
-	Нужно проверить может ли он купить новый MacBook за 2000$?
-	Он может брать не только свои деньги, но и взять кредит.
-	Ему дадут 500$, только если ему больше 24-х лет и он
-	имеет работу, 100$ если ему просто больше 24-х лет и 0 в
-	ином случае.
-	Напишите функцию, которая принимает данные пользователя
-	и товара и возвращает true или false;
+	Дан список задач
+	const tasks = ['Задача 1'];
+	Сделать функции:
+	- Добавление задачи в конец
+	- Удаление задачи по названию
+	- Перенос задачи в начало списка по названию 
+	Всегда меняем исходный массив
 */
-
-// 1 кридитная 2 проверяющая true или false на преобритение macbook
-
-function computerCredit(age, hasJob = false) {
-	switch(true) {
-		case age > 24 && hasJob:
-			return 500;
-		case age > 24:
-			return 100;
-		default:
-			return 0;
-	}	
-}
-
-function canBuy(productPrice, age, money, hasJob = false) {
-	const creditMoney = computerCredit(age, hasJob);
-	return productPrice <= money + creditMoney;
-}
-
-console.log(canBuy(2000, 25, 1900));
