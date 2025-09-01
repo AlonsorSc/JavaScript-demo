0d83dc2a34c3f73f464395608957e7b96377c050
Nikita <altorego1@yandex.ru>
2025-09-01 21:51:23 +0300

    master - промежуточный коммит сохраняем результат упражнения

--- 1 - Упражнения/app.js
+++ 1 - Упражнения/app.js
@@ -7,3 +7,38 @@
 	- Перенос задачи в начало списка по названию 
 	Всегда меняем исходный массив
 */
+const tasks = ['Задача 1'];
+
+function createTask(newTask) {
+	tasks.push(newTask);
+	return tasks;
+}
+
+function removeTask(numberTask) {
+	const index = tasks.indexOf(numberTask);
+	if (index !== -1) {
+		tasks.splice(index, 1);
+	} else {
+		console.log('Задача не найдена');
+	}
+	return tasks;
+}
+
+function moveTaskToStart(taskName) {
+	const index2 = tasks.indexOf(taskName);
+	if (index2 >= 0) {
+		tasks.splice(index2, 1);
+		tasks.unshift(taskName);
+	}
+	return tasks;
+}
+
+// добавление задач в конец
+console.log(createTask('Задача 2'));
+console.log(createTask('Задача 3'));
+
+// // удаление задачи по имени
+console.log(removeTask('Задача 2'));
+
+// перемещение задачи в начало списка
+console.log(moveTaskToStart('Задача '));
